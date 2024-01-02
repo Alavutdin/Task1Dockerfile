da70e70be43cac5d9349715e64611a0700ca687d
Alavutdin <alavutdin_atlan@mail.ru>
2024-01-02 22:13:23 +0300

    Initial commit

---
+++ src/Task1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        char operator;
+        Double number1, number2, result;
+
+        // create an object of Scanner class
+        Scanner input = new Scanner(System.in);
+
+        // ask User to enter operator
+        System.out.println("Choose an operator: +, -, *, or /");
+        operator = input.next().chars(0);
+
+
+
+    }
+}
